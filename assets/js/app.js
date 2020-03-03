2f735570e0e3ee21930f1cd35c90e0483b1119dd
Kianimal <spoono17@gmail.com>
2020-03-03 16:16:49 -0600

    Successfully pulling and appending still images to the page. Styling is god awful.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,48 @@
-var buttonList = [];
+var buttonList = ["Apex Legends",
+                  "PUBG",
+                  "Fortnite",
+                  "Destiny 2",
+                  "Escape From Tarkov"
+                ];
 
-var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=iOVspgT4s9daQGKag345gmCFIKM39WRV";
+function createButtons(arr) {
+  for(i=0; i<arr.length; i++){
+    var btnDiv = $("<div>");
+    btnDiv.attr("class","btnMenu");
+    btnDiv.text(arr[i]);
+    var content = document.getElementById("btnContainer");
+    $(content).append(btnDiv);
+    console.log("inside loop");
+  }
+}
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
-    });
+createButtons(buttonList);
+
+var clicked = document.getElementsByClassName("btnMenu");
+
+$(clicked).click(function(){
+  console.log(this.innerText);
+  var searchTerm = this.innerText;
+  searchTerm = searchTerm.toLowerCase().trim().split(' ').join('+');
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=iOVspgT4s9daQGKag345gmCFIKM39WRV";
+               // http://api.giphy.com/v1/gifs/search?q=    ryan+gosling  &api_key=YOUR_API_KEY&limit=5
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    var content = document.getElementById("btnContainer");
+    var stillImg = response.data[0].images.original_still.url;
+    $(content).append("<img src ='" + stillImg + "'>");
+  });
+});
+
+// // var queryURL = "https://api.giphy.com/v1/gifs/?" +  + "&api_key=iOVspgT4s9daQGKag345gmCFIKM39WRV";
+
+//     $.ajax({
+//       url: queryURL,
+//       method: "GET"
+//     }).then(function(response) {
+//       var content = document.getElementById("btnContainer");
+//       var stillImg = response.data[0].images.original_still.url;
+//       // $(content).append("<img src ='" + stillImg + "'>");
+    // });
