80ce49c23888b86620cbb09a6e638bad12388e97
Kianimal <spoono17@gmail.com>
2020-03-07 01:04:42 -0600

    Changed display area styling. Slightly larger, with less area between GIFs. Blur now less intense on 'censored' images. Some minor cleaning up of js code.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,30 @@
+//Array to hold button labels
 var buttonList = ["Apex Legends",
                   "PUBG",
                   "Fortnite",
                   "Destiny 2",
                   "Escape From Tarkov",
                 ];
+//Variables to hold dynamically generated content
+var urlStill = []; // Holds all URLs for still images
+var urlAnim = [];  // Holds all URLs for GIFs
+var idCounter = 0; // A unique numerical ID for all images starting at 0
+var limit = 5;     // The default limit of GIFs to display
 
-var reqCount = 10;
-var btnSubmit = document.getElementById("btnSubmit");
-var urlStill = [];
-var urlAnim = [];
-var idCounter = 0;
-var limit = 5;
+//Initial button loadout on pageload
+createButtons(buttonList);
 
 //Function to render buttons from buttonList array
 function createButtons(arr) {
   $("#btnWrap").empty();
   for(i=0; i<arr.length; i++){
-    var btnDiv = $("<div>");
+    var btnDiv = $("<button>");
     btnDiv.attr("class","btnMenu");
     btnDiv.text(arr[i]);
     var content = document.getElementById("btnWrap");
     $(content).append(btnDiv);
   }
-}
-
-//Initial button loadout on pageload
-createButtons(buttonList);
+};
 
 //Functionality for form submission and button re-creation
 $("form").submit(function(event){
@@ -39,10 +38,10 @@ function changeLimit(event){
   var newLimit = event.value;
   limit = newLimit;
   console.log(limit);
-}
+};
 
 //Functionality to animate GIF
-$(document.body).on("click",".gif",function(){
+$(document).on("click",".gif",function(){
   if($(this).prop("rating") == "PG-13" || $(this).prop("rating") == "R") {    
     $(this).toggleClass("img-blur",false);
   };
@@ -68,7 +67,7 @@ $(document.body).on("click",".gif",function(){
 });
 
 // Functionality for menu buttons to append GIFs
-$(document.body).on("click",".btnMenu",function(){
+$(document).on("click",".btnMenu",function(){
   var searchTerm = this.innerText;
   searchTerm = searchTerm.toLowerCase().trim().split(' ').join('+');
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=iOVspgT4s9daQGKag345gmCFIKM39WRV";
